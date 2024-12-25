58d323397920d48bbb21e3a4be73fda1488dcc0e
wolfTeachcake <160516107+wolfTeachcake@users.noreply.github.com>
2024-12-25 17:08:43 +0800

    updt

--- choice-time-for-sales/src/App.js
+++ choice-time-for-sales/src/App.js
@@ -1,16 +1,20 @@
 import TimerContainer from './component/timer-container/timer-container.component';
 import ScheduleContainer from './component/schedule-container/schedule-container.component';
 import CompleteTime from './component/complete-time/complete-time.component';
-
+import styled from 'styled-components';
+const ContainerStyle = styled.div`
+	width: 90vw;
+	margin: 0 auto;
+`;
 function App() {
 	return (
-		<div className="App">
-			<h1>首通確認</h1>
+		<ContainerStyle className="App">
+			<h1>導覽確認</h1>
 			<h3>請至少選出三個可以時段</h3>
 			<TimerContainer />
 			<ScheduleContainer />
 			<CompleteTime />
-		</div>
+		</ContainerStyle>
 	);
 }
 
