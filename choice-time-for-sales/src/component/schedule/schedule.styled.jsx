29f563d7524ab80f118171571e39ddaa965d83c6
wolfTeachcake <160516107+wolfTeachcake@users.noreply.github.com>
2025-03-03 01:35:32 +0800

    Delete choice-time-for-sales directory

--- choice-time-for-sales/src/component/schedule/schedule.styled.jsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const TitleName = styled.span`
-	white-space: nowrap;
-`;
--- choice-time-for-sales/src/component/timer-container/timer-container.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FlexContainer = styled.div`
-	display: flex;
-	gap: 20px;
-`;
-export const TitleName = styled.span`
-	white-space: nowrap;
-`;
