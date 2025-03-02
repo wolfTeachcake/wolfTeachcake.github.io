e24c5b93d6d4383f195bc090a6ce5587dac3a452
wolfTeachcake <160516107+wolfTeachcake@users.noreply.github.com>
2025-03-03 01:46:18 +0800

    update

---
+++ choice-time-for-sales/src/component/complete-time/complete-time.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const FlexContainer = styled.div`
+	display: flex;
+	width: 90%;
+	margin: 0 auto;
+	gap: 20px;
+	margin-bottom: 10px;
+`;
+export const TitleName = styled.span`
+	white-space: nowrap;
+`;
